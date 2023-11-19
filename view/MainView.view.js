cde0fe9e1f430d794c410b8606272bb8660f365e
Ardra S M <84071302+Ardra-18@users.noreply.github.com>
2023-11-19 23:33:50 +0530

    Add files via upload

---
+++ view/MainView.view.js
@@ -0,0 +1,16 @@
+// MainView.view.js
+sap.ui.define([
+    "sap/ui/core/mvc/XMLView"
+  ], function (XMLView) {
+    "use strict";
+  
+    return XMLView.extend("com.ui5learners.view.MainView", {
+      metadata: {
+        // Define metadata if needed
+      },
+      init: function () {
+        // Initialization logic for the view
+      }
+    });
+  });
+  
