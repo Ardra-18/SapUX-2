a2d56981c2e088b7aee98eb92eefb2be67974606
Ardra S M <84071302+Ardra-18@users.noreply.github.com>
2023-11-19 23:35:17 +0530

    Add files via upload

---
+++ controller/MainController.js
@@ -0,0 +1,15 @@
+// MainController.js
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+  ], function (Controller) {
+    "use strict";
+  
+    return Controller.extend("com.ui5learners.controller.MainController", {
+      onInit: function () {},
+      onBeforeRendering: function(){},
+      onAfterRendering: function() {},
+      onExit: function(){},
+      // Additional controller functions can be added as needed
+    });
+  });
+  
